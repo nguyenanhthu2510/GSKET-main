e3b77dfe6b93c6455873ae7bdc806880a06cd747
nguyenanhthu2510 <thunat2510@gmail.com>
2024-08-20 08:56:35 +0700

    update efficientnetb3

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ epochs=(100 500 1000)
 # Loop through each epoch
 for e in "${epochs[@]}"
 do
-    python main_knowledge.py --cfg config/iu_retrieval.yml --gpu 0 --version 1 --epoch "$e" --visual_extractor "swin" --batch_size 32
+    python main_knowledge.py --cfg config/iu_retrieval.yml --gpu 0 --version 1 --epoch "$e" --visual_extractor "efficientnet" --batch_size 32
 
 done
